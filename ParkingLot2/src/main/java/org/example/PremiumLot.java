4b124528ac7252ecc7c750d831bd29ad6141a322
Shurun Liu <ireneliusr@gmail.com>
2023-10-30 16:18:28 +0000

    premiumLotCommit

--- ParkingLot2/src/main/java/org/example/PremiumLot.java
+++ ParkingLot2/src/main/java/org/example/PremiumLot.java
@@ -13,6 +13,35 @@ public class PremiumLot implements ParkingLot {
 //        Daily max is $27
 //        Every 7th day or fraction thereof is free.  (ie, a stay of 13 days and 4 hours will be billed for 12 days
 
-        return 0;
+        int days = ticket.getDays();
+        int hours = ticket.getHours();
+        int mins = ticket.getMinutes();
+        int freeDays = days/7;
+        int actualBilledDays = 0;
+
+        int dailyRate = 27;
+        int hourlyRate =4;
+        int cost = 0;
+
+        if (ticket.getDays() >= 7) {
+            if (ticket.getHours() >= 7) {
+                actualBilledDays = ticket.getDays() + 1;
+                cost = ((actualBilledDays - freeDays) * dailyRate);
+            }
+            return cost;
+        } else if (ticket.getHours() < 7) {
+            if (ticket.getMinutes() == 0) {
+                actualBilledDays = ticket.getDays();
+                cost = ((actualBilledDays - freeDays) * dailyRate) + ticket.getHours() * hourlyRate;
+            } else {
+                cost = ((actualBilledDays - freeDays) * dailyRate) + (ticket.getHours() * hourlyRate) + hourlyRate;
+            }
+            return cost;
+        } else {
+            cost = (ticket.getDays() * dailyRate) + (ticket.getHours() * hourlyRate);
+            return cost;
+        }
+
     }
+
 }
---
+++ ParkingLot2/src/test/java/org/example/PremiumParkingTest.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class PremiumParkingTest {
+    PremiumLot uut;
+    Ticket mockTicket;
+
+    @BeforeEach
+    void setUp() {
+        mockTicket = Mockito.mock(Ticket.class);
+        uut = new PremiumLot();
+    }
+
+    @AfterEach
+    void tearDown() {
+        uut = null;
+    }
+
+    double expectedResult;
+    double actualResult;
+    LocalDateTime entryTime,exitTime;
+
+    @ParameterizedTest
+    @CsvSource({"0,1,4","1,1,8","6,0,24","7,0,27","7,1,27"})
+    void testFirstDay(int hour, int minutes, double cost) {
+        when(mockTicket.getDays()).thenReturn(0);
+        when(mockTicket.getMinutes()).thenReturn(minutes);
+        when(mockTicket.getHours()).thenReturn(hour);
+        assertEquals(cost,uut.calculateFee(mockTicket));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({"9,0,0,216","11,1,1,278","15,0,1,355"})
+    void testFreeDays(int days,int hours, int minutes, double cost) {
+        when(mockTicket.getDays()).thenReturn(days);
+        when(mockTicket.getMinutes()).thenReturn(minutes);
+        when(mockTicket.getHours()).thenReturn(hours);
+        assertEquals(cost,uut.calculateFee(mockTicket));
+    }
+
+//
+//    @ParameterizedTest
+//    @CsvSource({"1,9,1,35","0,11,1,15","2,8,1,48"})
+//    void testGeneralCost(int days,int hrs,int min, double cost) {
+//        when(mockTicket.getDays()).thenReturn(days);
+//        when(mockTicket.getMinutes()).thenReturn(min);
+//        when(mockTicket.getHours()).thenReturn(hrs);
+//        assertEquals(cost,uut.calculateFee(mockTicket));
+//    }
+
+
+}
